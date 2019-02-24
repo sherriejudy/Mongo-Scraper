110bc2530a56956cf0860401c0076af46ae86c30
Judy Hu <judy.hu9@gmail.com>
2019-02-23 20:02:13 -0800

    Reverting to scraping arts page

--- config/apiRoutes.js
+++ config/apiRoutes.js
@@ -5,67 +5,44 @@ var db = require("../models");
 module.exports = function(app) {
   //scrape articles
   app.get("/scrape", function(req, res) {
-    axios.get("https://thenewspaper.ca/").then(function(axios_response) {
-      var $ = cheerio.load(axios_response.data);
-      var titles = [];
-      var summaries = [];
-      var links = [];
-      var pages = $("ul#menu-navigation-1 li.menu-item-type-taxonomy a");
-      var categories = [];
-      // Iterate through the pages
-      for (var i = 0; i < pages.length; i++) {
-        let url = $(pages[i]).attr("href");
-        // Get current page
-        axios.get(url).then(response => {
-          $ = cheerio.load(response.data);
-          $("h3.entry-title.td-module-title a").each((i, element) => {
-            titles.push($(element).text());
-          });
-          $(".td-excerpt").each((i, element) => {
-            var split = $(element)
-              .text()
-              .split("\n");
-            for (var j = 0; j < split.length; j++) {
-              if (split[i] === "") {
-                split.splice(i, 1);
-              }
-            }
-            summaries.push(split);
-          });
-          $(".td-module-image .td-module-thumb a").each((i, element) => {
-            links.push($(element).attr("href"));
-          });
-          var successfullyScrapedPage = false;
-          for (var i = 0; i < summaries.length; i++) {
-            var obj = {};
-            obj.title = titles[i];
-            obj.summary = summaries[i];
-            obj.link = links[i];
-            obj.saved = false;
-            console.log("Trying to find" + obj.title);
-            db.Article.find({ link: obj.link })
-              .then(article => {
-                console.log("found" + obj.title);
-                // Check if article exists already. If it does, skip this page.
-              })
-              .catch(err => {
-                console.log("Error:" + err + ":" + obj.title);
-              });
-            db.Article.create(obj, function(err, res) {
-              successfullyScrapedPage = true;
-              if (err) {
-                console.log("Judy Error: " + err);
-              }
-            });
-          }
-          if (successfullyScrapedPage) {
-            // Return if a page has been scraped
-            return;
-          }
+    axios
+      .get("https://thenewspaper.ca/category/arts/")
+      .then(function(axios_response) {
+        var $ = cheerio.load(axios_response.data);
+        var titles = [];
+        var summaries = [];
+        var links = [];
+        $("h3.entry-title.td-module-title a").each((i, element) => {
+          titles.push($(element).text());
         });
-      }
-      res.redirect("/");
-    });
+        $(".td-excerpt").each((i, element) => {
+          var split = $(element)
+            .text()
+            .split("\n");
+          for (var j = 0; j < split.length; j++) {
+            if (split[i] === "") {
+              split.splice(i, 1);
+            }
+          }
+          summaries.push(split);
+        });
+        $(".td-module-image .td-module-thumb a").each((i, element) => {
+          links.push($(element).attr("href"));
+        });
+        for (var i = 0; i < summaries.length; i++) {
+          var obj = {};
+          obj.title = titles[i];
+          obj.summary = summaries[i];
+          obj.link = links[i];
+          obj.saved = false;
+          db.Article.create(obj, function(err, res) {
+            if (err) {
+              console.log("Judy Error: " + err);
+            }
+          });
+        }
+        res.redirect("/");
+      });
   });
   // save article
   app.post("/api/saved/:id", function(req, res) {
